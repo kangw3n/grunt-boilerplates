d25b255c0d4e9fd88b8f213e79134d75af8c9592
kangw3n <weitochearyeah@hotmail.com>
2015-06-23 16:32:25 +0800

    Redo all grunt task

---
+++ dist/js/script.js
@@ -0,0 +1,2 @@
+/*---------kangw3n 2015-----------*/
+$("button").on("click",function(){});
--- gruntfile.js
+++ gruntfile.js
@@ -1,54 +1,96 @@
 module.exports = function(grunt) {
+  // load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
+  require('load-grunt-tasks')(grunt);
+
   grunt.initConfig({
     watch: {
       sass: {
         files: 'dev/sass/**/*.scss',
-        tasks: ['sass']
+        tasks: ['compass', 'purifycss']
       },
       script: {
-        files: ['dev/js/script.js'],
-        tasks: ['uglify']
+        files: ['dev/js/*.js'],
+        tasks: ['uglify', 'purifycss']
+      },
+      html: {
+        files: ['dev/**/*.html'],
+        tasks: ['htmlmin', 'purifycss']
       }
 
     },
     sass: {
       dev: {
         files: {
-          'client/css/style.css': 'dev/sass/style.scss'
+          // OUTPUT_PATH : SOURCES_PATH
+          'dev/css/style.css': 'dev/sass/style.scss'
         }
+      }
+    },
+    compass: { // Task
+      dist: { // Target
+        options: { // Target options
+          config: 'config.rb'
+        }
+      }
+    },
+    imagemin: {
+      dynamic: { // Another target
+        options: { // Target options
+          optimizationLevel: 3,
+          progressive: true,
+          svgoPlugins: [{
+            removeViewBox: false
+          }]
+        },
+        files: [{
+          expand: true, // Enable dynamic expansion
+          cwd: 'dev/', // Src matches are relative to this path
+          src: ['**/*.{png,jpg,gif}'], // Actual patterns to match
+          dest: 'dist/' // Destination path prefix
+        }]
       }
     },
     requirejs: {
       compile: {
         options: {
-          appDir: 'client/',
+          appDir: 'dev/',
           baseUrl: 'js',
           dir: 'dist',
-          mainConfigFile: 'client/js/script.js',
-          name: 'script', // assumes a production build using almond
-          uglify2: {
-            output: {
-              beautify: false
-            },
-            warning: true,
-            mangle: true //replace var name
-          },
+          mainConfigFile: 'dev/js/script.js',
+          name: 'lib/almond/almond', // assumes a production build using almond
+          // uglify2: {
+          // output: {
+          // beautify: false
+          // },
+          // warning: true,
+          // mangle: true //replace var name
+          // },
           preserveLicenseComments: false
         }
+      }
+    },
+    purifycss: {
+      options: {
+        minify: true
+      },
+      target: {
+        src: ['dist/**/*.html', 'dist/js/*.js'], // Observed files
+        css: ['dev/css/*.css'], // Take all css files into consideration
+        dest: 'dist/css/style.css' // Write to this path
       }
     },
     browserSync: {
       dev: {
         bsFiles: {
           src: [
-            'client/css/*.css',
-            'client/js/*.js',
-            'client/*.html'
+            'dist/css/*.css',
+            'dist/js/*.js',
+            'dist/**/*.html'
           ]
         },
         options: {
           watchTask: true,
-          server: './client',
+          server: './dist',
           ghostMode: {
             clicks: true,
             forms: true,
@@ -65,30 +107,41 @@ module.exports = function(grunt) {
       },
       my_target: {
         files: {
-          'client/js/script.js': 'dev/js/script.js'
+          'dist/js/script.js': 'dev/js/script.js'
         }
       }
     },
-    jshint: {
-      options: {
-        eqeqeq: true,
-        curly: true
-      },
-      target: {
-        src: 'dev/js/*.js'
+    htmlmin: { // Task
+      dist: { // Target
+        options: { // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: [{ // Dictionary of files
+          expand: true,
+          cwd: 'dev',
+          src: '**/*.html', // 'destination': 'source'
+          dest: 'dist'
+        }]
+      }
+    },
+    wiredep: {
+      task: {
+        // Point to the files that should be updated when
+        // you run `grunt wiredep`
+        src: [
+          'dev/**/*.html'
+        ],
+        options: {
+          // See wiredep's configuration documentation for the options
+          // you may pass:
+
+          // https://github.com/taptapship/wiredep#configuration
+        }
       }
     }
   });
 
-  // load npm tasks
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-browser-sync');
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
-
   // define default task
   grunt.registerTask('default', ['browserSync', 'watch']);
-  grunt.registerTask('require', ['requirejs']);
 };
